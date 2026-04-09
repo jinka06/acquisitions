b9970b80c7b56cd4e00beeca0a4f43b0dae420b0
jinka06 <jeremie.inkura@gmail.com>
2026-04-09 14:44:58 +0200

    Setup of the logger and integration of helmet for headers response and morgan as a middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import { createUsersTable } from './models/user.model.js';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 await createUsersTable();
 
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) }}));
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions');
   res.status(200).send('Everything is ok within the API');
 });
 

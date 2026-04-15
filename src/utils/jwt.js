a3001d1c715a3bfc1b33ae7d16ab1d2dd8cb807b
jinka06 <jeremie.inkura@gmail.com>
2026-04-15 14:10:50 +0200

    Small fix on the jwt error handling

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -9,7 +9,7 @@ export const jwttoken = {
         try{
             return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN});
         } catch(e){
-            logger.error('Failed to authenticate token', error);
+            logger.error('Failed to authenticate token', e);
             throw new Error('Failed to authenticate token');
         }
     },
@@ -17,7 +17,7 @@ export const jwttoken = {
         try{
             return jwt.verify(token, JWT_SECRET);
         } catch(e){
-            logger.error('Failed to authenticate token', error);
+            logger.error('Failed to authenticate token', e);
             throw new Error('Failed to authenticate token');
         }
     }
